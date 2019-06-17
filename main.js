73b4e5af461f48b3074c5fd8d0c83137444375b5
TabbyKat123 <51475285+TabbyKat123@users.noreply.github.com>
2019-06-17 11:24:56 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,20 +3,20 @@ var q = Math.floor(Math.random()*100)+1;
 var h = q%2;
 
 if (h==0)
-	alert(q+"number is even");
+	alert("number is even");
 else
-	alert(q+"number is odd");
+	alert("number is odd");
 
 for (count=1;count<=10;count++){
 	let num= prompt("please input any number:");
-	if (num == 1){
+	if (num == q){
 		alert("You Win")
 		break;
 	}
-	if (num<h){
+	if (num<q){
 		alert("Number is lower, "+"please try again.");
 	}
-	if (num>h){
+	if (num>q){
 		alert ("Number is higher, "+ "please try again.")
 	}
 	if (count ==10){
